e3bcb905cca76f090ee4ea86aee782687c71ca56
Equan P. <equan.p@gmail.com>
2024-07-25 08:01:59 +0700

    add settings

--- apps/server.js
+++ apps/server.js
@@ -1,11 +1,23 @@
+import path from 'path'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { __dirname } from './dirname.js'
 
 const app = express()
+
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3000
+if (!process.env.VITE_APP_URL) {
+	throw new Error('VITE_APP_URL environment variable is not set')
+}
+
+// eslint-disable-next-line no-undef
+const apiURL = new URL(process.env.VITE_APP_URL)
+const HOSTNAME = apiURL.hostname || 'localhost'
+const PORT = apiURL.port || 4000
 
 app.use(bodyParser.json({ limit: '10mb' }))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.post('/process-image', (req, res) => {
 	const { image } = req.body
@@ -28,6 +40,6 @@ app.post('/process-image', (req, res) => {
 	res.json(simulatedResponse)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, HOSTNAME, () => {
 	console.log(`Server is running on http://localhost:${PORT}`)
 })
