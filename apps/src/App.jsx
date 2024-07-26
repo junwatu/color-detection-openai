57b7e8629d3c5f311f0df5b4a42d0d03f7e5683d
Equan P. <equan.p@gmail.com>
2024-07-26 10:13:53 +0700

    Add color palettes component

--- apps/src/App.jsx
+++ apps/src/App.jsx
@@ -1,11 +1,18 @@
-import WebcamContainer from './WebcamContainer';
-import Chatbox from './Chatbox';
+import Chatbox from './Chatbox'
+import WebcamContainer from './WebcamContainer'
+import BoxContainer from './BoxContainer'
+import ColorRectangles from './ColorRectangle'
 
 const App = () => {
+	const colors = ['#2A2C9B', '#F08A7D', '#8E5DB2', '#E8A1A3', '#4D3B9E', '#7F3C8F', '#B57AB3']
+
 	return (
 		<div id="content-container">
 			<WebcamContainer />
 			<Chatbox />
+			<BoxContainer>
+				<ColorRectangles colors={colors} />
+			</BoxContainer>
 		</div>
 	)
 }
